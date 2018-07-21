6fc286c7f2d656cb833c60f7cd07225beeaab339
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-21 12:03:43 +0100

    Chal8

---
+++ lib/chal8.rb
@@ -0,0 +1,20 @@
+require "set"
+
+class Chal8
+  def likely_ecb?(sample)
+    n = 0
+    seen = Set[]
+    while true
+      slice = sample[n, 16]
+      break if slice.size != 16
+      return true if seen.include?(slice)
+      seen << slice
+      n += 16
+    end
+    false
+  end
+
+  def call(samples)
+    samples.select{|sample| likely_ecb?(sample)}.map(&:pack_hex)
+  end
+end
