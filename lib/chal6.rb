0f7743731723cdc86046937611f284d2d1ed9658
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-21 13:01:50 +0100

    Half of Chal6

--- lib/chal6.rb
+++ lib/chal6.rb
@@ -3,4 +3,22 @@ class Chal6
     raise unless str1.size == str2.size
     str1.unpack("C*").zip(str2.unpack("C*")).map{|u,v| (u^v).count_bits}.sum
   end
+
+  def repeated_edit_distance(str, n)
+    i = 0
+    total = 0
+    while true
+      break if 2*n + i > str.size
+      str1 = str[i, n]
+      str2 = str[i+n, n]
+      total += hamming_distance(str1, str2)
+      i += n
+    end
+    # Divide it here, to avoid
+    total / i.to_f
+  end
+
+  def guess_keysize(str)
+    (2..40).min_by{|k| repeated_edit_distance(str, k) }
+  end
 end
--- spec/chal6_spec.rb
+++ spec/chal6_spec.rb
@@ -1,4 +1,7 @@
 describe Chal6 do
+  let(:sample_path) { Pathname("#{__dir__}/data/6.txt") }
+  let(:sample) { Base64.decode64(sample_path.read) }
+
   describe "#hamming_distance" do
     let(:a) { "this is a test" }
     let(:b) { "wokka wokka!!!" }
@@ -9,13 +12,14 @@ describe Chal6 do
   end
 
   describe "#repeated_edit_distance" do
-
     it do
-
+      expect(Chal6.new.repeated_edit_distance(sample, 40)).to eq(3.18)
     end
   end
 
   describe "#guess_keysize" do
-
+    it do
+      expect(Chal6.new.guess_keysize(sample)).to eq(29)
+    end
   end
 end
