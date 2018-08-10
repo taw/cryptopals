fb6db12d9d58ead719a90a4d8b035799c26d22ac
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-08-10 21:41:31 +0100

    Chal 49 - server for second part

--- lib/chal49.rb
+++ lib/chal49.rb
@@ -5,18 +5,12 @@ class Chal49
     end
 
     def call(request)
-      message, iv, mac = parse_request(request)
-      from_id, to_id, amount = parse_message(message)
-      raise "Invalid MAC" unless cbc_mac(message, iv) == mac
+
       # Everythig is good, so execute!
-      ["OK", from_id, to_id, amount]
+      ["OK", *parse_message(parse_request(request))]
     end
 
     private
-
-    def key_for(account)
-      @keys[account] or raise "Account #{account} not found"
-    end
 
     def parse_message(message)
       raise "Invalid message: #{message.inspect}" unless message =~ /\Afrom=(\d+)&to=(\d+)&amount=(\d+)\z/
@@ -29,7 +23,8 @@ class Chal49
       mac = request[-16..-1]
       iv = request[-32..-17]
       message = request[0..-33]
-      [message, iv, mac]
+      raise "Invalid MAC" unless cbc_mac(message, iv) == mac
+      message
     end
 
     def cbc_mac(msg, iv)
@@ -52,16 +47,64 @@ class Chal49
     end
   end
 
-   def self.cbc_mac(msg, key, iv)
-      AES.encrypt_cbc(msg, key, iv)[-16..-1]
-   end
+  def self.hack(request, account1, account2)
+    account1 = account1.to_s
+    account2 = account2.to_s
+    raise "Attack only works if both accounts have same length" unless account1.size == account2.size
+    mask = "from=#{account1}".xor("from=#{account2}")
+    raise "Attack only works on first block" if mask.size > 16
+    request.xor(mask + "\x00".b * (request.size - 32 - mask.size) + mask + "\x00".b * (32-mask.size))
+  end
 
-   def self.hack(request, account1, account2)
-      account1 = account1.to_s
-      account2 = account2.to_s
-      raise "Attack only works if both accounts have same length" unless account1.size == account2.size
-      mask = "from=#{account1}".xor("from=#{account2}")
-      raise "Attack only works on first block" if mask.size > 16
-      request.xor(mask + "\x00".b * (request.size - 32 - mask.size) + mask + "\x00".b * (32-mask.size))
-   end
- end
+  class Server2
+    def initialize(key)
+      @key = key
+    end
+
+    def call(request)
+      # Everythig is good, so execute!
+      ["OK", *parse_message(parse_request(request))]
+    end
+
+    private
+
+    def parse_message(message)
+      raise "Invalid message: #{message.inspect}" unless message =~ /\Afrom=(\d+)&tx_list=(.*)\z/
+      [$1.to_i, *$2.split(";").map{|tx| tx.split(":", 2).map(&:to_i) }.flatten]
+    end
+
+    def parse_request(request)
+      # Require at least one byte
+      raise "Request too short" unless request.size >= 33
+      mac = request[-16..-1]
+      message = request[0..-17]
+      raise "Invalid MAC" unless cbc_mac(message) == mac
+      message
+    end
+
+    def cbc_mac(msg)
+      Chal49.cbc_mac(msg, @key, "\x00".b*16)
+    end
+  end
+
+  class WebClient2
+    def initialize(account, key)
+      @account = account
+      @key = key
+    end
+
+    def generate_transfer_request(from_id, *txs)
+      raise "Can only sign messages from own account" unless from_id == @account
+      txs = txs.map(&:to_i)
+      raise "Malformatted tx list" unless txs.size.even? and txs.size >= 2
+      txs = txs.each_slice(2).map{|tx| tx.join(":")}.join(";")
+      msg = "from=#{from_id}&tx_list=#{txs}"
+      mac = Chal49.cbc_mac(msg, @key, "\x00".b*16)
+      (msg + mac)
+    end
+  end
+
+  def self.cbc_mac(msg, key, iv)
+    AES.encrypt_cbc(msg, key, iv)[-16..-1]
+  end
+end
--- spec/chal49_spec.rb
+++ spec/chal49_spec.rb
@@ -1,35 +1,57 @@
 describe Chal49 do
-  let(:key) { AES.random_key }
-  let(:server) { Chal49::Server.new(key) }
-  let(:client1) { Chal49::WebClient.new(100, key) }
-  let(:client2) { Chal49::WebClient.new(200, key) }
-  let(:client3) { Chal49::WebClient.new(300, key) }
+  describe "IV attack" do
+    let(:key) { AES.random_key }
+    let(:server) { Chal49::Server.new(key) }
 
-  # PART 1
-  describe "Every cliet can sign only own requests" do
-    let(:req1_good) { client1.generate_transfer_request(100, 200, 1000) }
-    let(:req2_good) { client2.generate_transfer_request(200, 300, 2000) }
-    let(:req1_bad) { client2.generate_transfer_request(100, 200, 3000) }
-    let(:req2_bad) { client1.generate_transfer_request(200, 300, 4000) }
+    describe "Every client can sign only own requests" do
+      let(:client1) { Chal49::WebClient.new(100, key) }
+      let(:client2) { Chal49::WebClient.new(200, key) }
+      let(:req1_good) { client1.generate_transfer_request(100, 200, 1000) }
+      let(:req2_good) { client2.generate_transfer_request(200, 300, 2000) }
+      let(:req1_bad) { client2.generate_transfer_request(100, 200, 3000) }
+      let(:req2_bad) { client1.generate_transfer_request(200, 300, 4000) }
 
-    it do
-      expect(server.call(req1_good)).to eq(["OK", 100, 200, 1000])
-      expect(server.call(req2_good)).to eq(["OK", 200, 300, 2000])
-      expect{ req1_bad }.to raise_error("Can only sign messages from own account")
-      expect{ req2_bad }.to raise_error("Can only sign messages from own account")
+      it do
+        expect(server.call(req1_good)).to eq(["OK", 100, 200, 1000])
+        expect(server.call(req2_good)).to eq(["OK", 200, 300, 2000])
+        expect{ req1_bad }.to raise_error("Can only sign messages from own account")
+        expect{ req2_bad }.to raise_error("Can only sign messages from own account")
+      end
+    end
+
+    # Target - 100
+    # Attacker - 300/400s
+    describe "Requests can be faked" do
+      let(:client3) { Chal49::WebClient.new(300, key) }
+      let(:req) { client3.generate_transfer_request(300, 400, 1_000_000) }
+      let(:hacked) { Chal49.hack(req, 100, 300) }
+      it do
+        expect(server.call(hacked)).to eq(["OK", 100, 400, 1_000_000])
+      end
     end
   end
 
-  # Target - 100
-  # Attacker - 300/400s
-  describe "Requests can be faked" do
-    let(:req) { client3.generate_transfer_request(300, 400, 1_000_000) }
-    let(:hacked) { Chal49.hack(req, 100, 300) }
-    it do
-      expect(server.call(hacked)).to eq(["OK", 100, 400, 1_000_000])
+  describe "Length extension attack" do
+    let(:key) { AES.random_key }
+    let(:server) { Chal49::Server2.new(key) }
+
+    describe "Every client can sign only own requests" do
+      let(:client1) { Chal49::WebClient2.new(100, key) }
+      let(:client2) { Chal49::WebClient2.new(200, key) }
+      let(:req1_good) { client1.generate_transfer_request(100, 200, 1000, 300, 2000) }
+      let(:req2_good) { client2.generate_transfer_request(200, 300, 2000, 400, 5000) }
+      let(:req1_bad) { client2.generate_transfer_request(100, 200, 3000, 300, 2000) }
+      let(:req2_bad) { client1.generate_transfer_request(200, 300, 4000, 400, 5000) }
+      it do
+        expect(server.call(req1_good)).to eq(["OK", 100, 200, 1000, 300, 2000])
+        expect(server.call(req2_good)).to eq(["OK", 200, 300, 2000, 400, 5000])
+        expect{ req1_bad }.to raise_error("Can only sign messages from own account")
+        expect{ req2_bad }.to raise_error("Can only sign messages from own account")
+      end
+    end
+
+    describe "Requests can be faked" do
+      pending
     end
   end
-
-  # PART 2
-  pending
 end
