d3a2c22fee53e9ef83781cd531139f1b5c0b54b4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-08-11 08:40:51 +0100

    #extend_block_collision

--- lib/chal52.rb
+++ lib/chal52.rb
@@ -1,39 +1,50 @@
 class Chal52
-  # 16bit
-  class F
-    def initial_state
-      "ABCD"
-    end
-
-    def reduce(state, chunk)
-      AES.encrypt_block(chunk, state + "\x00"*12)[0, 4]
-    end
-
+  class IteratedHash
     def hexdigest(msg)
       AES.pad(msg).byteslices(16).reduce(initial_state) do |state, chunk|
         reduce(state, chunk)
       end.to_hex
+    end
+
+    def find_block_collision(state)
+      hashes = {}
+      i = 0
+      while true
+        chunk = "%016d" % i
+        h = reduce(state, chunk)
+        if hashes[h]
+          return [hashes[h], chunk, h]
+        end
+        hashes[h] = chunk
+        i += 1
+      end
+    end
+
+    def extend_block_collision(state, *collisions)
+      a, b, new_state = find_block_collision(state)
+      [new_state, *collisions, [a, b]]
+    end
+  end
+
+  # 16bit
+  class F < IteratedHash
+    def initial_state
+      "AB"
+    end
+
+    def reduce(state, chunk)
+      AES.encrypt_block(chunk, state + "\x00"*14)[0, 2]
     end
   end
 
   # 24bit
-  class G
+  class G < IteratedHash
     def initial_state
-      "012345"
+      "012"
     end
 
     def reduce(state, chunk)
-      AES.encrypt_block(chunk, state + "\x00"*10)[0, 6]
-    end
-
-    def finalize(state)
-      state.to_hex
-    end
-
-    def hexdigest(msg)
-      AES.pad(msg).byteslices(16).reduce(initial_state) do |state, chunk|
-        reduce(state, chunk)
-      end.to_hex
+      AES.encrypt_block(chunk, state + "\x00"*13)[0, 3]
     end
   end
 
--- spec/chal52_spec.rb
+++ spec/chal52_spec.rb
@@ -1,9 +1,39 @@
 describe Chal52 do
+  let(:f) { Chal52::F.new }
+  let(:g) { Chal52::G.new }
+  let(:fg) { Chal52::FG.new }
+
   describe "Hashes" do
     it do
-      expect(Chal52::F.new.hexdigest("Hello, world!")).to eq("b99f9769")
-      expect(Chal52::G.new.hexdigest("Hello, world!")).to eq("e1df55cda0cc")
-      expect(Chal52::FG.new.hexdigest("Hello, world!")).to eq("b99f9769" + "e1df55cda0cc")
+      expect(f.hexdigest("Hello, world!")).to eq("53fe")
+      expect(g.hexdigest("Hello, world!")).to eq("111507")
+      expect(fg.hexdigest("Hello, world!")).to eq("53fe" + "111507")
     end
+  end
+
+  describe "#find_block_collision" do
+    it do
+      c1, c2, h = f.find_block_collision(f.initial_state)
+      expect(c1).to_not eq(c2)
+      expect(f.hexdigest(c1)).to eq(f.hexdigest(c2))
+    end
+  end
+
+  describe "#extend_block_collision" do
+    it do
+      state_and_collisions = [f.initial_state]
+      4.times do
+        state_and_collisions = f.extend_block_collision(*state_and_collisions)
+      end
+      all_msgs = state_and_collisions[1..-1].reduce([""]){|msgs,(a,b)| msgs.flat_map{|msg| [msg+a, msg+b] }  }
+      hashes = all_msgs.map{|msg| f.hexdigest(msg) }
+      expect(all_msgs.size).to eq(16)
+      expect(all_msgs.uniq.size).to eq(16)
+      expect(hashes.uniq.size).to eq(1)
+    end
+  end
+
+  describe "hack" do
+    pending
   end
 end
