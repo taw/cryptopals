dda9cf521fd3ff2e32f36027002a4fb5e65884e4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-09-04 20:21:42 +0100

    ECDSA

---
+++ lib/ecdsa.rb
@@ -0,0 +1,95 @@
+module ECDSA
+  class Group < Struct.new(:curve, :g, :n)
+  end
+
+  class PrivateKey < Struct.new(:group, :q, :d)
+    def public_key
+      PublicKey.new(group, q)
+    end
+
+    def g
+      group.g
+    end
+
+    def n
+      group.n
+    end
+
+    def curve
+      group.curve
+    end
+
+    def sign(msg)
+      k = rand(2...n)
+      h = ECDSA.hash(msg)
+      r = curve.multiply(g, k)[0] % n
+      s = ((h + d*r) * k.invmod(n)) % n
+      Signature.new(public_key, msg, r, s)
+    end
+
+    def self.generate_key(group)
+      curve = group.curve
+      g = group.g
+      n = group.n
+      d = rand(2...n)
+      PrivateKey.new(group, curve.multiply(g, d), d)
+    end
+  end
+
+  class PublicKey < Struct.new(:group, :q)
+    def curve
+      group.curve
+    end
+
+    def g
+      group.g
+    end
+
+    def n
+      group.n
+    end
+  end
+
+  class Signature < Struct.new(:public_key, :msg, :r, :s)
+    def group
+      public_key.group
+    end
+
+    def n
+      group.n
+    end
+
+    def curve
+      group.curve
+    end
+
+    def q
+      public_key.q
+    end
+
+    def g
+      public_key.g
+    end
+
+    def valid?
+      return false unless 0 < r and r < n
+      return false unless 0 < s and s < n
+      inv_s = s.invmod(n)
+      u1 = (h * inv_s) % n
+      u2 = (r * inv_s) % n
+      rr = curve.add( curve.multiply(g, u1), curve.multiply(q, u2) )[0] % n
+      rr == r
+    end
+
+    def h
+      ECDSA.hash(msg)
+    end
+  end
+
+  class << self
+    def hash(msg)
+      # 120 bit
+      Digest::SHA1.hexdigest(msg).to_i(16) & 0xffffffffffffffffffffffffffffff
+    end
+  end
+end
---
+++ spec/chal61_spec.rb
@@ -0,0 +1,36 @@
+describe Chal61 do
+  let(:prime) { 233970423115425145524320034830162017933 }
+  let(:curve) { WeierstrassCurve.new(prime, -95051, 11279326) }
+  let(:base_point) { [182, 85518893674295321206118380980485522083] }
+  let(:base_point_order) { 29246302889428143187362802287225875743 }
+  let(:group) { ECDSA::Group.new(curve, base_point, base_point_order) }
+
+  describe ECDSA do
+    describe "Sign and verify" do
+      let(:private_key) { ECDSA::PrivateKey.generate_key(group) }
+      let(:public_key) { private_key.public_key }
+      let(:msg1) { "Hello world!" }
+      let(:msg2) { "Goodbye world!" }
+      let(:sig1) { private_key.sign(msg1) }
+      let(:sig2) { private_key.sign(msg2) }
+
+      it "real ones" do
+        expect(sig1).to be_valid
+        expect(sig2).to be_valid
+      end
+
+      it "some fake ones" do
+        expect(ECDSA::Signature.new(public_key, msg1, sig1.r, sig1.s)).to be_valid
+        expect(ECDSA::Signature.new(public_key, msg1, sig2.r, sig1.s)).to_not be_valid
+        expect(ECDSA::Signature.new(public_key, msg1, sig1.r, sig2.s)).to_not be_valid
+        expect(ECDSA::Signature.new(public_key, msg1, sig2.r, sig2.s)).to_not be_valid
+      end
+    end
+  end
+
+  # ECDSA hack
+  pending
+
+  # RSA hack
+  pending
+end
