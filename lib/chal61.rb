b74b5ca04c83870a79ba75939966e758708df780
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-09-05 18:04:27 +0100

    ECDSA attack part of Chal 61

--- lib/chal61.rb
+++ lib/chal61.rb
@@ -1,2 +1,30 @@
 class Chal61
+  def create_fake_ecda_signature_key(signature)
+    group = signature.group
+    curve = group.curve
+    public_key = signature.public_key
+    q = public_key.q
+    g = group.g
+    n = group.n
+    s = signature.s
+    r = signature.r
+    msg = signature.msg
+    h = ECDSA.hash(msg)
+    inv_s = s.invmod(n)
+    u1 = (h * inv_s) % n
+    u2 = (r * inv_s) % n
+    rr = curve.add( curve.multiply(g, u1), curve.multiply(q, u2) )
+
+    new_d = rand(2...n)
+    new_t = (u1 + u2*new_d) % n
+    new_g = curve.multiply(rr, new_t.invmod(n))
+    new_q = curve.multiply(new_g, new_d)
+
+    new_group = ECDSA::Group.new(curve, new_g, n)
+    new_private_key = ECDSA::PrivateKey.new(new_group, new_q, new_d)
+    new_public_key = new_private_key.public_key
+    new_signature = ECDSA::Signature.new(new_public_key, msg, r, s)
+
+    [new_private_key, new_signature]
+  end
 end
--- spec/chal61_spec.rb
+++ spec/chal61_spec.rb
@@ -1,4 +1,5 @@
 describe Chal61 do
+  let(:chal) { Chal61.new }
   let(:prime) { 233970423115425145524320034830162017933 }
   let(:curve) { WeierstrassCurve.new(prime, -95051, 11279326) }
   let(:base_point) { [182, 85518893674295321206118380980485522083] }
@@ -29,7 +30,39 @@ describe Chal61 do
   end
 
   # ECDSA hack
-  pending
+  describe "create_fake_ecda_signature_key" do
+    let(:private_key) { ECDSA::PrivateKey.generate_key(group) }
+    let(:public_key) { private_key.public_key }
+    let(:msg) { "Hello world!" }
+    let(:legit_signature) { private_key.sign(msg) }
+
+    # New key, on same curve but with different valid generator
+    it do
+      private_key, hacked_signature = chal.create_fake_ecda_signature_key(legit_signature)
+      public_key = hacked_signature.public_key
+      # It is valid
+      expect(hacked_signature).to be_valid
+
+      # We control it
+      expect(hacked_signature.public_key).to eq(private_key.public_key)
+
+      # It's mostly matching
+      expect(hacked_signature.r).to eq(legit_signature.r)
+      expect(hacked_signature.s).to eq(legit_signature.s)
+      expect(hacked_signature.msg).to eq(legit_signature.msg)
+      expect(hacked_signature.public_key.curve).to eq(legit_signature.public_key.curve)
+      expect(hacked_signature.public_key.n).to eq(legit_signature.public_key.n)
+
+      # Differences
+      expect(hacked_signature.public_key).to_not eq(legit_signature.public_key)
+      expect(hacked_signature.public_key.g).to_not eq(legit_signature.public_key.g)
+      expect(hacked_signature.public_key.q).to_not eq(legit_signature.public_key.q)
+
+      # It is still a generator
+      expect(public_key.curve.multiply(public_key.g, public_key.n)).to eq(:infinity)
+    end
+  end
+
 
   # RSA hack
   pending
