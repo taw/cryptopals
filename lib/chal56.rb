9d33b5b2d422ed4f17345ba38f15ef8f295ef60e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-09-15 11:22:48 +0100

    Chal56 (only quick version in repo)

--- lib/chal56.rb
+++ lib/chal56.rb
@@ -40,6 +40,12 @@ class Chal56
   end
 
   class Attacker
+    KNOWN_BIASES = [
+      [ 1,   0, 2**16],
+      [15, 240, 2**24],
+      [31, 224, 2**24],
+    ]
+
     def initialize(box)
       @box = box
     end
@@ -48,8 +54,32 @@ class Chal56
       @cookie_len ||= @box[""].size
     end
 
-    def attack(box)
-      cookie_len
+    def best_bias_for(i)
+      KNOWN_BIASES.find{|j,| j >= i} or raise "Unknown bias at position #{i}"
+    end
+
+    def attack_byte(target_ofs)
+      bias_ofs, bias_value, count = best_bias_for(target_ofs)
+      pad = "\x00".b * (bias_ofs - target_ofs)
+      table = [0] * 256
+      count.times do
+        ctb = @box[pad][bias_ofs].ord
+        table[ctb] += 1
+      end
+      # v1 is actual answer, rest just for debugging
+      (most_common_freq, most_common_value), (most_common_freq2, most_common_value2) = table.each_with_index.sort.reverse
+      v1 = (bias_value ^ most_common_value).chr
+      v2 = (bias_value ^ most_common_value2).chr
+      r = most_common_freq.to_f / most_common_freq2
+      [v1, v2, r]
+    end
+
+    def attack
+      cookie_len.times.map do |target_ofs|
+        b1, b2, ratio = attack_byte(target_ofs)
+        # puts "Found: #{b1.inspect} #{b2.inspect} #{ratio} - at #{target_ofs}"
+        b1
+      end.join
     end
   end
 end
--- spec/chal56_spec.rb
+++ spec/chal56_spec.rb
@@ -27,7 +27,8 @@ describe Chal56 do
   end
 
   describe "attack" do
-    let(:cookie) { Base64.decode64("QkUgU1VSRSBUTyBEUklOSyBZT1VSIE9WQUxUSU5F") }
+    let(:full_cookie) { Base64.decode64("QkUgU1VSRSBUTyBEUklOSyBZT1VSIE9WQUxUSU5F") }
+    let(:cookie) { full_cookie }
     let(:box) { chal.box(cookie) }
     let(:attacker) { Chal56::Attacker.new(box) }
 
@@ -35,6 +36,27 @@ describe Chal56 do
       expect(attacker.cookie_len).to eq(cookie.size)
     end
 
-    pending
+    it "#best_bias_for" do
+      expect(attacker.best_bias_for(0)).to eq(Chal56::Attacker::KNOWN_BIASES[0])
+      expect(attacker.best_bias_for(1)).to eq(Chal56::Attacker::KNOWN_BIASES[0])
+      expect(attacker.best_bias_for(2)).to eq(Chal56::Attacker::KNOWN_BIASES[1])
+      expect(attacker.best_bias_for(15)).to eq(Chal56::Attacker::KNOWN_BIASES[1])
+      expect(attacker.best_bias_for(16)).to eq(Chal56::Attacker::KNOWN_BIASES[2])
+      expect{ attacker.best_bias_for(32) }.to raise_error "Unknown bias at position 32"
+    end
+
+    it "#attack_byte" do
+      expect(attacker.attack_byte(0)[0]).to eq(cookie[0])
+      expect(attacker.attack_byte(1)[0]).to eq(cookie[1])
+    end
+
+    describe "#attack" do
+      # Full attack is very slow, so put only short version in specs
+      let(:cookie) { full_cookie[0,3] }
+
+      it do
+        expect(attacker.attack).to eq(cookie)
+      end
+    end
   end
 end
