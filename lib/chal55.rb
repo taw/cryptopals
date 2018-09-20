d9cdf9649877f809f2aa5ed3f75f9b543e51733e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-09-20 23:29:34 +0100

    Generate candidate pairs with correct round1 conditions

--- lib/chal55.rb
+++ lib/chal55.rb
@@ -1,15 +1,32 @@
 class Chal55
+  MASK = (1 << 32) - 1
+
+  # a b c d
+  INITIAL_STATE = [
+    0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476,
+  ].freeze
+
+  ROTATIONS = [
+    3,7,11,19, 3,7,11,19, 3,7,11,19, 3,7,11,19,
+    3,5,9,13, 3,5,9,13, 3,5,9,13, 3,5,9,13,
+    3,9,11,15, 3,9,11,15, 3,9,11,15, 3,9,11,15,
+  ]
+
+  SCHEDULE = [
+    0,1,2,3, 4,5,6,7, 8,9,10,11, 12,13,14,15,
+    0,4,8,12, 1,5,9,13, 2,6,10,14, 3,7,11,15,
+    0,8,4,12, 2,10,6,14, 1,9,5,13, 3,11,7,15,
+  ]
+
+  def self.rotate_left(v, s)
+    (v << s).&(MASK) | (v.&(MASK) >> (32 - s))
+  end
+
+  def self.rotate_right(v, s)
+    rotate_left(v, 32-s)
+  end
+
   class IntrospectiveMD4
-    ROTATIONS = [
-      3,7,11,19, 3,7,11,19, 3,7,11,19, 3,7,11,19,
-      3,5,9,13, 3,5,9,13, 3,5,9,13, 3,5,9,13,
-      3,9,11,15, 3,9,11,15, 3,9,11,15, 3,9,11,15,
-    ]
-    SCHEDULE = [
-      0,1,2,3, 4,5,6,7, 8,9,10,11, 12,13,14,15,
-      0,4,8,12, 1,5,9,13, 2,6,10,14, 3,7,11,15,
-      0,8,4,12, 2,10,6,14, 1,9,5,13, 3,11,7,15,
-    ]
 
     class << self
       def padding(message)
@@ -18,19 +35,9 @@ class Chal55
         "\x80".b + "\x00".b * extra_zeroes + [byte_size * 8].pack("Q<")
       end
 
-      def rotate_left(v, s)
-        mask = (1 << 32) - 1
-        (v << s).&(mask) | (v.&(mask) >> (32 - s))
-      end
-
-      def rotate_right(v, s)
-        rotate_left(v, 32-s)
-      end
-
       def reduce(state, chunk)
         x = chunk.unpack("V*")
         intermediate_values = []
-        mask = (1 << 32) - 1
 
         a, b, c, d = state
         aa, bb, cc, dd = a, b, c, d
@@ -39,7 +46,7 @@ class Chal55
           xi = x[SCHEDULE[j]]
           ri = ROTATIONS[j]
           if j <= 15
-            u = b & c | (b ^ mask) & d
+            u = b & c | (b ^ MASK) & d
             k = 0
           elsif j <= 31
             u = b & c | b & d | c & d
@@ -48,22 +55,22 @@ class Chal55
             u = b ^ c ^ d
             k = 0x6ed9eba1
           end
-          t = rotate_left(a + u + xi + k, ri)
+          t = Chal55.rotate_left(a + u + xi + k, ri)
           intermediate_values << t
           a, b, c, d = d, t, b, c
         end
 
         updated_state = [
-          (a + aa) & mask,
-          (b + bb) & mask,
-          (c + cc) & mask,
-          (d + dd) & mask,
+          (a + aa) & MASK,
+          (b + bb) & MASK,
+          (c + cc) & MASK,
+          (d + dd) & MASK,
         ]
         return [updated_state, intermediate_values]
       end
 
       def initial_state
-        [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
+        INITIAL_STATE
       end
 
       def finalize(state)
@@ -91,8 +98,8 @@ class Chal55
         {
           digest1: digest1,
           digest2: digest2,
-          message_diffs: v1.zip(v2).map{ |u,v| v-u },
-          intermediate_diffs: intermediate1.zip(intermediate2).map{ |u,v| v-u },
+          message_diffs: v1.zip(v2).map{ |u,v| (v-u) & MASK },
+          intermediate_diffs: intermediate1.zip(intermediate2).map{ |u,v| (v-u) & MASK },
         }
       end
     end
@@ -139,7 +146,7 @@ class Chal55
         when :e
           current[bit_ofs] == prev[bit_ofs]
         end
-        p [index, ok, type, current[bit_ofs], prev[bit_ofs]]
+        # p [index, ok, type, current[bit_ofs], prev[bit_ofs]]
         fails += 1 unless ok
       end
     end
@@ -147,20 +154,69 @@ class Chal55
   end
 
   def self.generate_candidate_pair
+    initial_state = Chal55::IntrospectiveMD4.initial_state
+
     v1 = 16.times.map{ rand(2**32) }
+    v1[1]  = v1[1].clear_bit(31)
+    v1[2]  = v1[2].clear_bit(31).set_bit(28)
+    v1[12] = v1[12].set_bit(16)
 
     # Round 1
     CONDITIONS.each_with_index do |conds, index|
+      conds.each do |type, bit_ofs|
+        digest, intermediate = Chal55::IntrospectiveMD4.reduce(initial_state, v1.pack("V*"))
+
+        states = [
+          initial_state[0],
+          initial_state[3],
+          initial_state[2],
+          initial_state[1],
+          *intermediate,
+        ]
+
+        if index == 0
+          # not even 100% sure actually
+          prev = initial_state[1]
+        else
+          prev = intermediate[index-1]
+        end
+        current = intermediate[index]
+        ok = case type
+        when :z
+          current[bit_ofs] == 0
+        when :o
+          current[bit_ofs] == 1
+        when :e
+          current[bit_ofs] == prev[bit_ofs]
+        end
+        unless ok
+          xi = v1[index]
+          ri = ROTATIONS[index]
+          a = states[index]
+          b = states[index+3]
+          c = states[index+2]
+          d = states[index+1]
+          tx = states[index+4]
+
+          u = b & c | (b ^ MASK) & d
+
+          t = rotate_left(a + u + xi, ri)
+          raise unless t == tx
+
+          fixed_t = t ^ (1 << bit_ofs)
+
+          fixed_xi = rotate_right(fixed_t, ri) - a - u
+
+          v1[index] = fixed_xi & MASK
+        end
+      end
     end
 
     # Generate pair with proper difference
     v2 = [*v1]
-    v2[1]  = v2[1].set_bit(31)
-    v1[1]  = v2[1].clear_bit(31)
-    v1[2]  = v1[2].clear_bit(31).set_bit(28)
-    v2[2]  = v2[2].set_bit(31).clear_bit(28)
-    v1[12] = v1[12].set_bit(16)
-    v2[12] = v2[12].clear_bit(16)
+    v2[1]  = v2[1] + (2**31)
+    v2[2]  = v2[2] + (2**31 - 2**28)
+    v2[12] = v2[12] - (2**16)
     [
       v1.pack("V*"),
       v2.pack("V*"),
