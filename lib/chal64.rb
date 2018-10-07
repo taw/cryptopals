d2b0ba7f25684b2384205da54cffc491ae7de367
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-10-07 23:36:25 +0100

    Build GF(2) 128x128 matrices for GCM

--- lib/chal64.rb
+++ lib/chal64.rb
@@ -1,9 +1,15 @@
 class Chal64
   def self.gcm_mul_matrix(c)
-    raise
+    c = GCMField.new(c)
+    GF2Matrix.build(128) do |i|
+      (c * GCMField.new(i)).value
+    end
   end
 
   def self.gcm_square_matrix
-    raise
+    GF2Matrix.build(128) do |i|
+      i = GCMField.new(i)
+      (i * i).value
+    end
   end
 end
--- spec/chal64_spec.rb
+++ spec/chal64_spec.rb
@@ -1,6 +1,15 @@
 describe Chal64 do
-  describe "Using GF2Matrix for GCM" do
-    pending
+  it "gcm_mul_matrix" do
+    a = rand(0...2**128)
+    b = rand(0...2**128)
+    ma = Chal64.gcm_mul_matrix(a)
+    expect(ma*b).to eq((GCMField.new(a) * GCMField.new(b)).value)
+  end
+
+  it "gcm_square_matrix" do
+    ms = Chal64.gcm_square_matrix
+    a = rand(0...2**128)
+    expect(ms*a).to eq((GCMField.new(a) * GCMField.new(a)).value)
   end
 
   pending
